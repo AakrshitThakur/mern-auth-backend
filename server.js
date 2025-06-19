224fc4e40d2015b6501e7e3464ca6ae0b4ca500a
AakrshitThakur <thakurraakrshitt@gmail.com>
2025-06-19 18:25:59 +0530

    Remote frontend url added for cors

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["https://mern-auth-frontend-static.netlify.app", "http://localhost:5173"],
     credentials: true,
   })
 );
